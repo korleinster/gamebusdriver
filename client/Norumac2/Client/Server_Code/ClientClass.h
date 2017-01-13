762171a22e804efd36b7a062f2970cf3303dc2e1
Uzchowall <khjkhj2804@naver.com>
2017-01-13 17:17:46 +0900

    [김형준] 서버 초기 진입 시, 다른 플레이어 정보를 받아오지 못하는 버그 수정 완료 ( 클라에서 패킷 재조립 코드 삽입 )

--- client/Norumac2/Client/Server_Code/ClientClass.h
+++ client/Norumac2/Client/Server_Code/ClientClass.h
@@ -30,7 +30,7 @@ private:
 	void error_quit(wchar_t *msg, int err_no);
 
 	SOCKET* getServerSocket();
-	void processPacket();
+	void processPacket(Packet*);
 
 	// 윈도우 창 관련 멤버 변수
 	WNDCLASS m_wndclass;
@@ -49,9 +49,10 @@ private:
 
 	Packet m_recvbuf[MAX_BUF_SIZE]{ 0 };
 	Packet m_sendbuf[MAX_BUF_SIZE]{ 0 };
+	Packet m_data_buf[MAX_BUF_SIZE]{ 0 };
 	unsigned int m_recvbytes{ 0 };
-	unsigned int m_sendbytes{ 0 };
-	bool m_recvdelayed{ 0 };
+	unsigned int m_packet_size_current{ 0 };
+	unsigned int m_packet_size_previous{ 0 };
 
 	// 실제 ingame에 사용할 멤버 변수 데이터
 	player_data m_player;

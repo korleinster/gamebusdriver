673c2e1d71fd21eb809c8f325a2f042d16f1cceb
korleinster <leinster92@me.com>
2017-02-27 18:16:33 +0900

    [신창섭]멥툴 작업중
    1.시바

--- client/MapTool/MapTool/StaticMesh.cpp
+++ client/MapTool/MapTool/StaticMesh.cpp
@@ -66,7 +66,7 @@ HRESULT CStaticMesh::Load_StaticMesh(const char* szFilePath, const char* szFileN
 	if (!pRootNode)
 		return E_FAIL;
 
-	
+
 
 	for (int i = 0; i < pRootNode->GetChildCount(); ++i)
 	{
@@ -107,7 +107,7 @@ HRESULT CStaticMesh::Load_StaticMesh(const char* szFilePath, const char* szFileN
 				++iVTXCounter;
 
 				vPos.x = (float)mControlPoints[iControlPointIndex].mData[0];
-				vPos.y = (float)mControlPoints[iControlPointIndex].mData[1];
+				vPos.y = -(float)mControlPoints[iControlPointIndex].mData[1];
 				vPos.z = (float)mControlPoints[iControlPointIndex].mData[2];
 
 				//uv ¾ò±â
@@ -120,14 +120,14 @@ HRESULT CStaticMesh::Load_StaticMesh(const char* szFilePath, const char* szFileN
 					case FbxGeometryElement::eDirect:
 					{
 						vOutUV.x = static_cast<float>(VtxUV->GetDirectArray().GetAt(iControlPointIndex).mData[0]);
-						vOutUV.y = 1.f - static_cast<float>(VtxUV->GetDirectArray().GetAt(iControlPointIndex).mData[1]);
+						vOutUV.y = static_cast<float>(VtxUV->GetDirectArray().GetAt(iControlPointIndex).mData[1]);
 					}
 					break;
 					case FbxGeometryElement::eIndexToDirect:
 					{
 						int index = VtxUV->GetIndexArray().GetAt(iControlPointIndex);
 						vOutUV.x = static_cast<float>(VtxUV->GetDirectArray().GetAt(index).mData[0]);
-						vOutUV.y = 1 - static_cast<float>(VtxUV->GetDirectArray().GetAt(index).mData[1]);
+						vOutUV.y = static_cast<float>(VtxUV->GetDirectArray().GetAt(index).mData[1]);
 					}
 					break;
 

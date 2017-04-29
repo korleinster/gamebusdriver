79aec0d4dc728d3a801e2d3547fedb34d0e0eb98
SoulfulGuRa <ahrl1213@naver.com>
2017-04-29 18:07:27 +0900

    [허지훈]Value
    상수들 추가

--- client/Norumac2/Client/Headers/Value.h
+++ client/Norumac2/Client/Headers/Value.h
@@ -13,6 +13,24 @@ const UINT VERTEXINTERVAL = 1;
 
 const D3DXVECTOR3 g_vLook(0.f, 0.f, 1.f);
 
+// Texture formats
+static const DXGI_FORMAT depthStencilTextureFormat = DXGI_FORMAT_R24G8_TYPELESS;
+static const DXGI_FORMAT basicColorTextureFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+static const DXGI_FORMAT normalTextureFormat = DXGI_FORMAT_R11G11B10_FLOAT;
+static const DXGI_FORMAT specPowTextureFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+// Render view formats
+static const DXGI_FORMAT depthStencilRenderViewFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+static const DXGI_FORMAT basicColorRenderViewFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+static const DXGI_FORMAT normalRenderViewFormat = DXGI_FORMAT_R11G11B10_FLOAT;
+static const DXGI_FORMAT specPowRenderViewFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+// Resource view formats
+static const DXGI_FORMAT depthStencilResourceViewFormat = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+static const DXGI_FORMAT basicColorResourceViewFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+static const DXGI_FORMAT normalResourceViewFormat = DXGI_FORMAT_R11G11B10_FLOAT;
+static const DXGI_FORMAT specPowResourceViewFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+
 #define VS_SLOT_VIEW_MATRIX			0x00
 #define VS_SLOT_PROJECTION_MATRIX	0x01
 #define VS_SLOT_WORLD_MATRIX		0x02

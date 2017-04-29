aced474f80f0358ce3873be4a54919239e5cc484
SoulfulGuRa <ahrl1213@naver.com>
2017-04-29 18:06:08 +0900

    [허지훈]RenderTarget
    변경

--- client/Norumac2/Client/Code/RenderTarget.cpp
+++ client/Norumac2/Client/Code/RenderTarget.cpp
@@ -1,8 +1,27 @@
 #include"stdafx.h"
 #include"RenderTarget.h"
 #include "Device.h"
+#include "ResourcesMgr.h"
+#include "VIBuffer.h"
+#include "ShaderMgr.h"
+#include "Shader.h"
+
+#pragma pack(push,1)
+struct CB_VS
+{
+	D3DXMATRIX m_mWorld;
+};
+#pragma pack(pop)
 
 CRenderTarget::CRenderTarget()
+	:m_pTargetRT(nullptr)
+	, m_pTagetRTV(nullptr)
+	, m_pTagetSRV(nullptr)
+	, m_pDebugBuffer(nullptr)
+	, m_pDebugBufferVS(nullptr)
+	, m_pDebugBufferPS(nullptr)
+	, m_pSamplerState(nullptr)
+	, m_pConstantBuffer(nullptr)
 {
 
 }
@@ -24,7 +43,7 @@ HRESULT CRenderTarget::Ready_RenderTarget(const  UINT& sizeX, const  UINT& sizeY
 		1, //DXGI_SAMPLE_DESC SampleDesc;
 		0,
 		D3D11_USAGE_DEFAULT,//D3D11_USAGE Usage;
-		D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE,//UINT BindFlags;
+		D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE,//UINT BindFlags;
 		0,//UINT CPUAccessFlags;
 		0//UINT MiscFlags;    
 	};
@@ -49,10 +68,90 @@ HRESULT CRenderTarget::Ready_RenderTarget(const  UINT& sizeX, const  UINT& sizeY
 		0,
 		0
 	};
+	dsrvd.Texture2D.MipLevels = 1;
 
 	CDevice::GetInstance()->m_pDevice->CreateShaderResourceView(m_pTargetRT, &dsrvd, &m_pTagetSRV);
 
 	return S_OK;
+}
+
+void CRenderTarget::Ready_DebugBuffer(const float& posX, const float& posY, const float& sizeX, const float& sizeY, const  UINT& type)
+{
+	//Clone DebugBuffer
+	m_pDebugBuffer = dynamic_cast<CVIBuffer*>(CResourcesMgr::GetInstance()->CloneResource(RESOURCE_STATIC, L"Buffer_RcTex"));
+
+	//Create Sampler State
+	D3D11_SAMPLER_DESC sampDesc;
+	ZeroMemory(&sampDesc, sizeof(sampDesc));
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.MinLOD = 0;
+	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	CDevice::GetInstance()->m_pDevice->CreateSamplerState(&sampDesc, &m_pSamplerState);
+
+	//Create Constant Buffer
+	D3D11_BUFFER_DESC cbd;
+	ZeroMemory(&cbd, sizeof(cbd));
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.ByteWidth = sizeof(CB_VS);
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	CDevice::GetInstance()->m_pDevice->CreateBuffer(&cbd, NULL, &m_pConstantBuffer);
+
+	//Calculate World Matrix
+	D3DXMATRIX matScale, matTransform;
+	D3DXMatrixIdentity(&m_matWorld);
+
+	D3DXMatrixScaling(&matScale, sizeX, sizeY, 1.f);
+	D3DXMatrixTranslation(&matTransform, posX, posY, 0.f);
+
+	m_matWorld = matScale * matTransform;
+
+	//Clone Shader
+	m_pDebugBufferVS = CShaderMgr::GetInstance()->Clone_Shader(L"DebugBufferVS");
+	if (type == 0)
+		m_pDebugBufferPS = CShaderMgr::GetInstance()->Clone_Shader(L"DebugBufferPS");
+	else if (type == 1)
+		m_pDebugBufferPS = CShaderMgr::GetInstance()->Clone_Shader(L"DebugBufferPS_Red");
+
+}
+
+void CRenderTarget::RenderRT(ID3D11DeviceContext* pd3dImmediateContext)
+{
+
+	D3D11_MAPPED_SUBRESOURCE MappedResource;
+	FAILED_CHECK_RETURN(pd3dImmediateContext->Map(m_pConstantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &MappedResource), );
+	CB_VS* pVSPerObject = (CB_VS*)MappedResource.pData;
+	D3DXMatrixTranspose(&pVSPerObject->m_mWorld, &m_matWorld);
+	pd3dImmediateContext->Unmap(m_pConstantBuffer, 0);
+
+	pd3dImmediateContext->IASetInputLayout(m_pDebugBufferVS->m_pVertexLayout);
+
+	pd3dImmediateContext->VSSetShader(m_pDebugBufferVS->m_pVertexShader, NULL, 0);
+	pd3dImmediateContext->VSSetConstantBuffers(0, 1, &m_pConstantBuffer);
+
+	pd3dImmediateContext->PSSetShader(m_pDebugBufferPS->m_pPixelShader, NULL, 0);
+	pd3dImmediateContext->PSSetShaderResources(0, 1, &m_pTagetSRV);
+	pd3dImmediateContext->PSSetSamplers(0, 1, &m_pSamplerState);
+
+	m_pDebugBuffer->Render();
+}
+
+void CRenderTarget::Release()
+{
+	Safe_Release(m_pTargetRT);
+	Safe_Release(m_pTagetRTV);
+	Safe_Release(m_pTagetSRV);
+
+	Safe_Delete(m_pDebugBuffer);
+	Safe_Delete(m_pDebugBufferVS);
+	Safe_Delete(m_pDebugBufferPS);
+
+	Safe_Release(m_pSamplerState);
+	Safe_Release(m_pConstantBuffer);
 }
 
 CRenderTarget* CRenderTarget::Create(const  UINT& sizeX, const  UINT& sizeY, DXGI_FORMAT format)
@@ -61,7 +160,7 @@ CRenderTarget* CRenderTarget::Create(const  UINT& sizeX, const  UINT& sizeY, DXG
 
 	if (FAILED(pInstance->Ready_RenderTarget(sizeX, sizeY, format)))
 	{
-		MessageBox(NULL,  L"System Message", L"RenderTarget Created Failed", MB_OK);
+		MessageBox(NULL, L"System Message", L"RenderTarget Created Failed", MB_OK);
 		Safe_Delete(pInstance);
 	}
 
--- client/Norumac2/Client/Code/RenderTarget.h
+++ client/Norumac2/Client/Code/RenderTarget.h
@@ -1,22 +1,39 @@
 #pragma once
 #include"Include.h"
 
+class CShader;
 class CRenderTarget
 {
 private:
 	explicit CRenderTarget();
 public:
 	virtual ~CRenderTarget();
+
+	void Release();
 public:
 	HRESULT Ready_RenderTarget(const  UINT&, const  UINT&, DXGI_FORMAT);
+	void Ready_DebugBuffer(const float& posX, const float& posY, const float& sizeX, const float& sizeY, const  UINT& type);
+	void RenderRT(ID3D11DeviceContext* pd3dImmediateContext);
+
+	ID3D11Texture2D* GetTexture() { return m_pTargetRT; }
+	ID3D11ShaderResourceView* GetSRV() { return m_pTagetSRV; }
+	ID3D11RenderTargetView* GetRTV() { return m_pTagetRTV; }
+
 private:
-
-
 	ID3D11Texture2D*			m_pTargetRT;
-	
 	ID3D11RenderTargetView*		m_pTagetRTV;
-	
 	ID3D11ShaderResourceView*	m_pTagetSRV;
+
+private:
+	class CVIBuffer*				m_pDebugBuffer;
+
+	D3DXMATRIX						m_matWorld;
+
+	CShader*						m_pDebugBufferVS;
+	CShader*						m_pDebugBufferPS;
+
+	ID3D11SamplerState*				m_pSamplerState;
+	ID3D11Buffer*					m_pConstantBuffer;
 
 public:
 	static CRenderTarget* Create(const  UINT&, const  UINT&, DXGI_FORMAT);

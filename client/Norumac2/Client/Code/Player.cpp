175e624620a4d6b86b4757a04f59a2c45edfedf5
Uzchowall <khjkhj2804@naver.com>
2017-05-10 19:40:04 +0900

    [김형준] 신창섭이 무능한 승필짜응이래

--- client/Norumac2/Client/Code/Player.cpp
+++ client/Norumac2/Client/Code/Player.cpp
@@ -21,6 +21,9 @@
 #include "ResourcesMgr.h"
 #include "OtherPlayer.h"
 #include "NaviMgr.h"
+#include "../Server_Code/ClientClass.h"
+
+extern AsynchronousClientClass g_client;
 
 #pragma pack(push,1)
 struct CB_VS_PER_OBJECT
@@ -80,7 +83,7 @@ HRESULT CPlayer::Initialize(void)
 	//m_ServerInfo.pos.y = m_pInfo->m_vPos.z;
 	//m_pInfo->m_vScale = D3DXVECTOR3(0.05f, 0.05f, 0.05f);
 	m_pInfo->m_vScale = D3DXVECTOR3(1.f, 1.f, 1.f);
-	m_fSpeed = 3.4;
+	m_fSpeed = 4.7f;
 
 	list<CObj*>::iterator iter = CObjMgr::GetInstance()->m_mapObj[L"Terrain"].begin();
 	list<CObj*>::iterator iter_end = CObjMgr::GetInstance()->m_mapObj[L"Terrain"].end();

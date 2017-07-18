f5b979c0f443ff8bcc92a0bfd7beb24b057e59dc
Uzchowall <khjkhj2804@naver.com>
2017-07-18 16:28:51 +0900

    [김형준] 청구서 & 불필요 코드 정리

--- server/2017 ChatClient/2017 Client/Client.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-
-	_wsetlocale(LC_ALL, L"korean");
-	win_main_class win_main_class(hInstance, nCmdShow);
-
-	return (int)win_main_class.getMsg()->wParam;
-}

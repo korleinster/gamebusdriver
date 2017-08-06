e7effb5a0ee6098748aa28655d8ea3763c51a8bf
SoulfulGuRa <ahrl1213@naver.com>
2017-08-06 21:13:57 +0900

    [허지훈]수정
    불필요 코드 삭제 및 그림자 품질

--- client/Norumac2/Client/Code/NPC.cpp
+++ client/Norumac2/Client/Code/NPC.cpp
@@ -173,14 +173,6 @@ void  CNpc::ShadowmapRender(void)
 {
 	if (m_bCurred == false)
 	{
-		/*D3D11_MAPPED_SUBRESOURCE MappedResource;
-		FAILED_CHECK_RETURN(m_pGrapicDevice->m_pDeviceContext->Map(m_pCascadedShadowGenVertexCB, 0, D3D11_MAP_WRITE_DISCARD, 0, &MappedResource), );
-		D3DXMATRIX* pVSPerObject = (D3DXMATRIX*)MappedResource.pData;
-		D3DXMatrixTranspose(pVSPerObject, &m_pInfo->m_matWorld);
-		m_pGrapicDevice->m_pDeviceContext->Unmap(m_pCascadedShadowGenVertexCB, 0);
-		m_pGrapicDevice->m_pDeviceContext->VSSetConstantBuffers(1, 1, &m_pCascadedShadowGenVertexCB);
-
-		dynamic_cast<CDynamicMesh*>(m_pBuffer)->PlayAnimation(0);*/
 		D3D11_MAPPED_SUBRESOURCE MappedResource;
 		FAILED_CHECK_RETURN(m_pGrapicDevice->m_pDeviceContext->Map(m_pCascadedShadowGenVertexCB, 0, D3D11_MAP_WRITE_DISCARD, 0, &MappedResource), );
 		D3DXMATRIX* pVSPerObject = (D3DXMATRIX*)MappedResource.pData;
